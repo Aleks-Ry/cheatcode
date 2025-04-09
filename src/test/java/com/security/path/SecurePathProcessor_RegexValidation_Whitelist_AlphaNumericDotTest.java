11874a3886d3574a7118c2508a9c35ffb654a9b9
Aleks-Ry <aleksei.ryzhkov@owasp.org>
2025-04-09 13:16:21 +1000

    Remove wrong tests

--- src/test/java/com/security/path/SecurePathProcessor_RegexValidation_Whitelist_AlphaNumericDotTest.java
+++ src/test/java/com/security/path/SecurePathProcessor_RegexValidation_Whitelist_AlphaNumericDotTest.java
@@ -13,41 +13,5 @@ class SecurePathProcessor_RegexValidation_Whitelist_AlphaNumericDotTest extends 
     @Override
     String getProcessorName() {
         return "Secure Path Processor (Regex Validation Whitelist)";
-    }
-
-    @Test
-    void LegitCase_AlphanumericFileName_ShouldReadFile() throws IOException {
-        ReadFileResult result = processor.readFile("legit123.txt");
-        assertFalse(result.IsPathTraversalAttackDetected);
-        assertFalse(result.IsPathSanitized);
-        assertEquals(PUBLIC_FILE_CONTENT, result.fileReadResult);
-        assertNull(result.fileReadException);
-    }
-
-    @Test
-    void InvalidCase_NonAlphanumericCharacters_ShouldFail() {
-        ReadFileResult result = processor.readFile("legit-file.txt");
-        assertTrue(result.IsPathTraversalAttackDetected);
-        assertTrue(result.IsPathSanitized);
-        assertNull(result.fileReadResult);
-        assertNotNull(result.fileReadException);
-    }
-
-    @Test
-    void InvalidCase_SpecialCharacters_ShouldFail() {
-        ReadFileResult result = processor.readFile("legit@file.txt");
-        assertTrue(result.IsPathTraversalAttackDetected);
-        assertTrue(result.IsPathSanitized);
-        assertNull(result.fileReadResult);
-        assertNotNull(result.fileReadException);
-    }
-
-    @Test
-    void InvalidCase_Spaces_ShouldFail() {
-        ReadFileResult result = processor.readFile("legit file.txt");
-        assertTrue(result.IsPathTraversalAttackDetected);
-        assertTrue(result.IsPathSanitized);
-        assertNull(result.fileReadResult);
-        assertNotNull(result.fileReadException);
-    }
+    }   
 }
